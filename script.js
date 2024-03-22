f05446c98b17fded9384f90c850eb605580d4633
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-03-23 00:58:38 +0330

    add new box to dom

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const $ = document
-
+const containerElem = $.querySelector('.container')
+const addNewElemHandle = $.querySelector('.add-new-note')
 const iconElemHandle = $.querySelector('.menu-svg')
 const menuElemHandle = $.querySelector('.menu-extend')
 
@@ -16,5 +17,30 @@ const menuHandler = () => {
     }
 }
 
+const addNewNote = () => {
+    let addNewNoteBox = `<div class="box">
+            <div class="head-box">
+                <h2>Learn Js</h2>
+                <p>I'll learn js from w3school</p>
+            </div>
+            <div class="foot-box">
+                <p class="date">April 12, 2024</p>
+                <img class="menu-svg" src="assets/ellipsis-solid.svg" width="40px" alt="menu-icon">
+                <div class="menu-extend">
+                    <span class="flex btn-edit">
+                        <img src="./assets/pen-solid.svg" width="15px" alt="">
+                        <h4>Edit</h4>
+                    </span>
+                    <span class="flex btn-delete">
+                        <img src="./assets/trash-solid.svg" width="15px" alt="">
+                        <h4>Delete</h4>
+                    </span>
+                </div>
+            </div>
+        </div>`
+
+        containerElem.insertAdjacentHTML("beforeend" ,addNewNoteBox)
+}
 
 iconElemHandle.addEventListener('click', menuHandler)
+addNewElemHandle.addEventListener('click', addNewNote)
